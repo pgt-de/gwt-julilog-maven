8ee055b6f7e4ab202d8a974c6fe9cc143126ac50
Papick Garcia Taboada <pgt@pgt.de>
2016-12-19 11:58:10 +0100

    Initial commit

---
+++ src/main/java/x/y/client/GreetingServiceAsync.java
@@ -0,0 +1,9 @@
+package x.y.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+public interface GreetingServiceAsync {
+    void greetServer(String name, AsyncCallback<String> async);
+}
